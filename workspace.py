6885f87553bf87ae7d87b6eda29dcc44dbf84a1e
Vj <vijeyyuvaraj@gmail.com>
2023-02-09 20:57:15 +0530

    feb9_assign_submission

--- workspace.py
+++ workspace.py
@@ -1,14 +1,3 @@
-inputStr = "I want to become a Data Scientist."
-def strEncrypt(i):
-    i = ord(i)-96;
-    return chr((26-i)+97)
-def encrypt(i):
-    if i == ' ':
-        return '$'
-    elif i == '.':
-        return i
-    else:
-        return strEncrypt(str.lower(i))
 
-for i in inputStr:
-    print(encrypt(i),end='')
+
+print(list(filter(None,[0,1,2,True,False,"","Vj"])))
